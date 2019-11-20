8a47863aadbd5ba584f7070f898f7a3c529821ac
LNorio <leonardocruz@alunos.utfpr.edu.br>
2019-11-19 22:22:19 -0300

    adicionando listar produtos e buscar

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProductsSearch from './ProductSearch'
+
+export default () => {
+    return (
+        <div>
+            <ProductsSearch name="kindred5"/>
+        </div>
+    )
+}
---
+++ src/components/ProductsList.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import '../assets/Products.css'
+
+export default class ProductsList extends Component {
+    state = {
+        products: []
+    };
+
+    getProductsFromApi = () => {
+        axios
+            .get("http://localhost:8080/prod", {
+                crossDomain: true
+            })
+            .then(result => {
+                this.setState({ products: result.data });
+            });
+    };
+
+    componentDidMount() {
+        this.getProductsFromApi();
+    }
+
+    sell(id){
+        console.log(id)
+    }
+
+    render() {
+        return (
+            <div className="lista">
+                {this.state.products.map(element => {
+                    console.log(element)
+                    return (
+                        <div className="prod" key={element._id}>
+                            <h2>{element.name}</h2>
+                            <img src={element.url} alt="produto"/>
+                            <h3>{element.price}</h3>
+                            <button onClick={() => this.sell(element._id)}>Vender</button>
+                        </div>    
+                    )
+                })}
+            </div>
+        )
+    }
+
+}
